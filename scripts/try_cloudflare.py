60e5a6048f31178d6f3529d51b9d59388b89b39c
Bingsu <ks2515@naver.com>
2023-02-09 22:33:36 +0900

    fix: port

--- scripts/try_cloudflare.py
+++ scripts/try_cloudflare.py
@@ -4,4 +4,5 @@ from modules.shared import cmd_opts
 
 if cmd_opts.cloudflared:
     print("cloudflared detected, trying to connect...")
-    try_cloudflare(cmd_opts.port)
+    port = cmd_opts.port if cmd_opts.port else 7860
+    try_cloudflare(port)
